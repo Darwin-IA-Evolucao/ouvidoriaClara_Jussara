f45f44e17e9d7f547c266b7cc30caaf1a953be61
ItaloBello <italo.bello10@gmail.com>
2026-08-27 17:24:17 -0300

    Adicionando Usuario

--- back-end/models/usuario.go
+++ back-end/models/usuario.go
@@ -1,6 +1,9 @@
 package models
 
 type Usuario struct {
+	ID      int    `db:"id" json:"id"`
 	Celular string `db:"celular" json:"celular"`
 	Senha   string `db:"senha" json:"senha"`
+	Role    string `db:"role" json:"role"`
+	Ativo   bool   `db:"ativo" json:"ativo"`
 }
